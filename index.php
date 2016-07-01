76b14abd1e27a6191661f97d2231588984a9b139
Éderson Sandre <ederson.sandre@gmail.com>
2016-07-01 16:40:47 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ederson
+ * Date: 01/07/2016
+ * Time: 16:15
+ */
+
+header('Content-Type: text/html; charset=utf-8');
+
+require 'vendor/autoload.php';
---
+++ src/BitOne/Gateway/HelloPackage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ederson
+ * Date: 01/07/2016
+ * Time: 16:27
+ */
+
+namespace BitOne\Gateway;
+
+
+class HelloPackage {
+
+    public function getHello(){
+        return "Hello";
+    }
+
+}
